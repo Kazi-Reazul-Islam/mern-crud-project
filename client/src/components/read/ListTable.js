8b9511e581566c04cc1721e26358b7a5396cbd5c
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-02-10 08:51:41 +0600

    list-table added

--- client/src/components/read/ListTable.js
+++ client/src/components/read/ListTable.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {Read} from "../../apiservices/CRUDServices";
+import FullScreenLoader from "../common/FullScreenLoader";
 
 const ListTable = () => {
     let [DataList,SetDataList] = useState([])
@@ -8,26 +9,56 @@ const ListTable = () => {
             SetDataList(Result)
         })
     },[])
-  return (
-    <div className="container mt-5">
-      <table className="table">
-          <thead>
-          <tr>
-              <th>Product Name</th>
-              <th>Product Code</th>
-              <th>Product Image</th>
-              <th>Product UnitPrice</th>
-              <th>Product Qty</th>
-              <th>TotalPrice</th>
-              <th>Action</th>
+    if(DataList.length>0){
+        return (
+            <div className="container mt-5">
+                <table className="table table-bordered ">
+                    <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Product Code</th>
+                        <th>Product Image</th>
+                        <th>Product UnitPrice</th>
+                        <th>Product Qty</th>
+                        <th>TotalPrice</th>
+                        <th>Action</th>
 
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        DataList.map((item,i)=>{
+                            return(
+                                <tr>
+                                    <td>{item.ProductName}</td>
+                                    <td>{item.ProductCode}</td>
+                                    <td><img className="list-img" src={item.Img}/></td>
+                                    <td>{item.UnitPrice}</td>
+                                    <td>{item.Qty}</td>
+                                    <td>{item.TotalPrice}</td>
+                                    <td>
+                                        <button className="btn btn-danger mx-2">Delete</button>
+                                        <button className="btn btn-primary">Update</button>
+                                    </td>
 
-          </tr>
-          <tbody></tbody>
-          </thead>
-      </table>
-    </div>
-  );
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+
+                </table>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+                <FullScreenLoader />
+            </div>
+        )
+    }
+
 };
 
 export default ListTable;
